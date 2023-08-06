00cdbe9180274e23c47f25b8cbcad1095927e92c
Iman-Basha <97814382+Iman-Basha@users.noreply.github.com>
2023-08-06 16:22:59 -0400

    data-filtering

--- data-filtering.py
+++ data-filtering.py
@@ -15,7 +15,6 @@ def remove_most_frequent_words_with_tag_O(data, most_frequent_words_O):
     return filtered_data
 
 def main():
-    # Load train, val, and test datasets from CSV files
     train_data = pd.read_csv('data/train.csv')
     val_data = pd.read_csv('data/val.csv')
     test_data = pd.read_csv('data/test.csv')
@@ -23,15 +22,11 @@ def main():
     # Load data.csv to identify the most frequent 400 words with tag "O"
     data_data = pd.read_csv('data/data.csv')
     most_frequent_words_O = data_data.loc[data_data['tag'] == 'O', 'Word'].str.lower().value_counts().nlargest(400).index
-
-    # Create '400_frequent_removed' directory if it doesn't exist
     create_directory_if_not_exists('data/400_frequent_removed')
-
     # Remove the most frequent 400 words with the tag "O" from train, val, and test datasets
     filtered_train_data = remove_most_frequent_words_with_tag_O(train_data, most_frequent_words_O)
     filtered_val_data = remove_most_frequent_words_with_tag_O(val_data, most_frequent_words_O)
     filtered_test_data = remove_most_frequent_words_with_tag_O(test_data, most_frequent_words_O)
-
     # Save the filtered datasets to CSV files
     filtered_train_data.to_csv('data/400_frequent_removed/filtered_train.csv', index=False)
     filtered_val_data.to_csv('data/400_frequent_removed/filtered_val.csv', index=False)
